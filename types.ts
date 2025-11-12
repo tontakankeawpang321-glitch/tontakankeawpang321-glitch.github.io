00931b0350b3d8528629e1c963ed9fc2b50e051c
tontakankeawpang <tontakankeawpang321@gmail.com>
2025-11-12 23:43:36 +0700

    Add files via upload

---
+++ types.ts
@@ -0,0 +1,14 @@
+
+export interface GroundingChunk {
+  web?: {
+    uri: string;
+    title: string;
+  };
+}
+
+export interface ChatMessage {
+  id: string;
+  role: 'user' | 'model';
+  text: string;
+  sources?: GroundingChunk[];
+}
