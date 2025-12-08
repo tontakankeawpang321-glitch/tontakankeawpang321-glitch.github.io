53b839c20e503ac8cd0ff10be911eb2664a125a4
tontakankeawpang <tontakankeawpang321@gmail.com>
2025-12-08 17:59:25 +0700

    Delete pali directory

--- pali/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.js';
-const container=document.getElementById('root');
-const root=ReactDOM.createRoot(container);
-root.render(React.createElement(React.StrictMode,null,React.createElement(App)));
